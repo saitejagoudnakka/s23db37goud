9f5c2bbb291a087ee0a0d9acec6d48d07124ae87
Sai Teja Goud Nakka <S565437@nwmissouri.edu>
2023-11-27 13:45:16 -0600

    Update recipe.js

--- routes/recipe.js
+++ routes/recipe.js
@@ -1,6 +1,14 @@
 var express = require('express');
 const recipe_controlers= require('../controllers/recipe');
 var router = express.Router();
+// A little function to check if we have an authorized user and continue on
+// redirect to login.
+const secured = (req, res, next) => {
+if (req.user){
+return next();
+}
+res.redirect("/login");
+}
 /* GET recipe */
 router.get('/', recipe_controlers.recipe_view_all_Page );
 /* GET detail recipe page */
@@ -8,7 +16,7 @@ router.get('/detail', recipe_controlers.recipe_view_one_Page);
 /* GET create recipe page */
 router.get('/create', recipe_controlers.recipe_create_Page);
 /* GET create update page */
-router.get('/update', recipe_controlers.recipe_update_Page);
+router.get('/update',secured, recipe_controlers.recipe_update_Page);
 /* GET delete recipe page */
 router.get('/delete', recipe_controlers.recipe_delete_Page);
 module.exports = router;
